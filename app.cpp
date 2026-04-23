8175e6e010efa615c26a2bef0707300390e04e79
kaiser-114 <areezilla@gmail.com>
2026-04-23 15:49:39 +0700

    menambahkan class LinkedList

--- app.cpp
+++ app.cpp
@@ -8,3 +8,23 @@ public:
     int noMhs;
     Node *next;
 };
+
+class LinkedList
+{
+     Node *START;
+
+
+     public:
+     LinkedList()
+     {
+        START = NULL;
+     }
+
+     void addNode()
+     {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
